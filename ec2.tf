735a754bbf8c70e693176c7c6a51a0b21f32898f
Bhaganagare122 <rushikeshbhaganagre121@gmail.com>
2025-09-01 15:10:44 +0530

    adding file

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-west-1"
+  
+}
+
+resource "aws_instance" "my-firstinstance" {
+    ami = "ami-0945610b37068d87a"
+    instance_type = "t2.micro"
+    tags = {
+      name = "webserver"
+    }
+}
